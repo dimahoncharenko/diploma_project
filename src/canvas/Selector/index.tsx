e491e254b967cf6617cdac347a34beb668e2ed48
dimahoncharenko <54737058+dimahoncharenko@users.noreply.github.com>
2023-12-22 14:47:21 +0200

    Delete src directory

--- src/canvas/Selector/index.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-
-type Props = {
-  children: ReactNode;
-};
-export function Selector({ children }: Props) {
-  return <group name="selector">{children}</group>;
-}
